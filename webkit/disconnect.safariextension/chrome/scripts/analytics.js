144b9c926c96640d874b091f6695835cdfb1018e
Gary Teh <garyjob@Garys-MacBook-Air.local>
2012-10-30 15:46:59 -0700

    removed out console output line

--- webkit/disconnect.safariextension/chrome/scripts/analytics.js
+++ webkit/disconnect.safariextension/chrome/scripts/analytics.js
@@ -17,7 +17,6 @@ analytics.prototype.triggerEvent = function(event, attr) {
   }
   
   url += '/?' + queryString;
-  console.log(url)
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
