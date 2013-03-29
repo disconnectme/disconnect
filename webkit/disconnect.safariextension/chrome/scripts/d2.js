92f1e02b5424e049e99a431cc8ae23802f1e9efb
Brian Kennish <byoogle@gmail.com>
2013-03-29 12:39:50 -0700

    Reconnected JS.

--- webkit/disconnect.safariextension/chrome/scripts/d2.js
+++ webkit/disconnect.safariextension/chrome/scripts/d2.js
@@ -1,92 +1,95 @@
 /*
-The javascript for the pay what you want page.
+  The script for a pay-what-you-want page.
 
-Copyright 2010-2013 Disconnect, Inc.
+  Copyright 2013 Disconnect, Inc.
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License along with
+  this program. If not, see <http://www.gnu.org/licenses/>.
 
-Authors (one per line):
+  Authors (one per line):
 
-Eason Goodale <eason.goodale@gmail.com>
+    Eason Goodale <eason.goodale@gmail.com>
+    Brian Kennish <byoogle@gmail.com>
 */
-
 $(function() {
-  var disconnectSlider = $('#disconnect-slider');
-  var charitySlider = $('#charity-slider');
-  var disconnectBox = $('#disconnect-amount');
-  var charityBox = $('#charity-amount');
-
-  /*Updates the displayed value box to reflect total payment amount changes.*/
+  /* Updates the displayed value box to reflect amount changes. */
   function updateValueBox() {
-    var total = $('#payment-total input[name=payment-group]:checked').val()
-    var charityTotal = total * (charitySlider.slider('option', 'value') / 100);
-    var disconnectTotal = total * (disconnectSlider.slider('option', 'value') / 100);
+    var total = $('#one :checked').val();
+    var disconnectTotal =
+        (total * (disconnectSlider.slider('option', 'value') / 100)).toFixed(2);
+    var charityTotal =
+        (total * (charitySlider.slider('option', 'value') / 100)).toFixed(2);
 
-    charityBox.val('$' + charityTotal);
-    disconnectBox.val('$' + disconnectTotal);
+    disconnectSplit.val('$' + disconnectTotal);
+    charitySplit.val('$' + charityTotal);
   }
 
+  var disconnectSlider = $('#disconnect-slider');
+  var charitySlider = $('#charity-slider');
+  var disconnectSplit = $('#disconnect-split');
+  var charitySplit = $('#charity-split');
+
   disconnectSlider.slider({
-    value: 100,
+    value: 75,
     orientation: 'horizontal',
     animate: true
   });
   charitySlider.slider({
-    value: 0,
+    value: 25,
     orientation: 'horizontal',
     animate: true
   });
 
   updateValueBox();
 
-  disconnectSlider.bind('slide',function() {
+  disconnectSlider.bind('slide', function() {
     var disconnectValue = disconnectSlider.slider('option', 'value');
     var charityValue = 100 - disconnectValue;
     charitySlider.slider('option', 'value', charityValue);
     updateValueBox();
   });
-  charitySlider.bind('slide',function() {
+  charitySlider.bind('slide', function() {
     var charityValue = charitySlider.slider('option', 'value');
     var disconnectValue = 100 - charityValue;
     disconnectSlider.slider('option', 'value', disconnectValue);
     updateValueBox();
   });
 
-  $('#custom-amount').change( function() {
-    $('input:radio[name=payment-group]')[4].checked = true;
-    $('#custom-radio').val($('#custom-amount').val());
+  $('#one input:radio').click(function() {
     updateValueBox();
   });
+  $('#override-price').change(function() {
+    $('#override')[0].checked = true;
+    var overrideValue = $(this).val();
 
-  $('input:radio[name=payment-group]').click( function() {
-    updateValueBox();
-  })
+    if ($.isNumeric(overrideValue) && overrideValue >= 0) {
+      $('#override').val(overrideValue);
+      updateValueBox();
+    }
+  });
 
   $('#stripe').click(function() {
-    var total = $('#payment-total input[name=payment-group]:checked').val()
-    var charityTotal = total * (charitySlider.slider('option', 'value') / 100);
-    var disconnectTotal = total * (disconnectSlider.slider('option', 'value') / 100);
-    
-    var token = function(res){
-      var $input = $('<input type="hidden" name="stripeToken">').val(res.id);
-      $('form').append($input).submit();
+    var total = $('#one :checked').val();
+
+    var token = function(result) {
+      var input = $('<input type="hidden" name="stripeToken">').val(result.id);
+      $('form').append(input).submit();
     };
 
     StripeCheckout.open({
-      //update this to our actual production key before it goes live
+      // TODO: Update to production key before going live.
       key:         'pk_test_czwzkTp2tactuLOEOqbMTRzG',
-      amount:      disconnectTotal * 100,
-      name:        'Disconnect Payment',
+      amount:      total * 100,
+      name:        'Give to Disconnect & charity',
       description: 'Thank you for your contribution!',
       token:       token
     });
