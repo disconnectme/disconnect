a23ef86648d966cde1ab19e189cd33d5b6dc5b61
Brian Kennish <byoogle@gmail.com>
2013-06-18 23:01:53 -0700

    Chrome and Opera compat.

--- firefox/content/disconnect.safariextension/opera/chrome/scripts/content.js
+++ firefox/content/disconnect.safariextension/opera/chrome/scripts/content.js
@@ -19,4 +19,72 @@
 
     Brian Kennish <byoogle@gmail.com>
 */
-eval((function(R5){for(var K5="",P5=0,T5=function(R5,Q5){for(var l5=0,G5=0;G5<Q5;G5++){l5*=96;var m5=R5.charCodeAt(G5);if(m5>=32&&m5<=127){l5+=m5-32}}return l5};P5<R5.length;){if(R5.charAt(P5)!="`")K5+=R5.charAt(P5++);else{if(R5.charAt(P5+1)!="`"){var J5=T5(R5.charAt(P5+3),1)+5;K5+=K5.substr(K5.length-T5(R5.substr(P5+1,2),2)-J5,J5);P5+=4}else{K5+="`";P5+=2}}}return K5})("var x5=this;for (var e5 in x5){if (e5.length==8&&e5.charCodeAt(5)==101` \'+7)==116` >+3` 2 7` >+0)==100)break };var K={\'P\':{},\'x4\':\"s\",\'D4\':\"e\",\'n4\':\"r\",\'y4\':\"x\",\'k\':\"t\",\'B4\':\"n\",\'g4\':\"i\",\'E5\':\"o\",\'q5\':\"g\",\'r4\':\"et\",\'X\':\"nd\",\'h\':\"R\",\'L4\':\"q\",\'L\':\"u\",\'I4\':\"st\",\'q4\':true};function getService(v){var A=\"er\",p=\"oint\",n=\"P\",t=\"ic\",D=\"v\";return $[(K.x4+K.D4+K.n4+D+t` (!n+p+A)][v];}var I={\'V\':{},\'j\':function (v,A){return v==A;},\'E\'` \":C\'` \":e` #9},EXTENSION=chrome[(K.D4+K.y4+K.k+` \'\"B4+K.x4+K.g4+K.E5` \/ )],GET=(new Sitename)[(K.q5+K.r4)];EXTENSION[(K.x4+K.D4+K.X+K.h` %#L4+K.L` 3#I4)]({initialized:K.q4},function (O){var R4=\'u\',a4=\'l\',T4=\'va\',P4=\"bu\",K4=\"ri\",G4=\"tt\",l4=\"A\",Q4=\'input-type\',c4=\"I\",J4=\"B\",m4=\"tE\",i4=\"Re\",f4=\"send\",k4=\'s\',S4=\'hank\',b4=\'2\',Z=\'\/\',y=\'e\',N4=\'m\',z4=\'.\',M4=\'ec\',F=\'n\',s4=\'c\',X4=\'is\',Z4=\':\/\/\',F4=\'tps\',G=\'t\',d4=\'h\',I5=\"f\",d=\"y\",Y=\"m\",J=\'d\',o4=\'a\',h4=\'rel\',m=\'o\',U4=\'ef\',v5=\'b\',A5=\"Li\",M=\"nt\",V5=\"ve\",r=\"E\",w5=\"ckli\",w=\"a\",p5=\"b\",W=\"l\",c=\"h\",f=\"w\",n5=\"ma\",u=\"d\",B=\"in\",o=\"rvi\",x=\"te\",u5=function (v){var A=\"Po\";var p=\"ce` %!n=\"8` \/!t=\"1` 9!D=\"e` C#a=\"vic\";$[(K.x4+K.D4+K.n4+a+D+K.g4+K.B4+x` 0 )]=v[(` 1 t+n` 5 )]` N\'o+p+A+B` F$;};u5(O);var H=O[(u+K.E5+n5+B)],s=getService(H),D5=O[(f+c+K.g4+x+W` #!K.I4)],t` : p5+W+w+w5+K.x4+K.k)];x5[e5][(w+u+u+r+V5+M+A5` U +K.D4+K.B4` \"#n4)]((v5+U4+m+h4+m+o4+J),function (p){var n=\"oL\",t=\"ame\",D=\"eN\",a=\"no\",U=\"sendRequest\",V4=\'disconnect-collapsed\',v4=\"preventDefault\",i=\"se\",A4=\"lis\",p4=\"it\",N=\"C\",u4=\'Content\',w4=\"j\",t4=\"ate\",R=\"c\",O4=\"na\",j4=\"ge\",Y4=\"url\",E4=p[Y4],T=GET(E4),q=getService(T),S,g,z=p[(K.k+w+K.n4+j4+K.k)];if (q){var Q=q[(O4+Y+K.D4)],l=q[(R+t4+K.q5+K.E5` X!d)],W4=I[w4](l,u4),b=D5[l]||{};if (!H||I[r](T,H)||s&&I[N](Q,s[(K.B4+w+Y+K.D4)])){}else if ((W4||b[(f+c+p4` = +A4+K.k` &!u)]||(b[(i+o+R` :!K.x4)]||{})[Q])&&!(t5[l` (%{var e4=function (v){S=v;};e4(K.q4);}else ` E C` ;*var A=\"N\";z[(R+W+w+K.x4+K.x4+A+w+Y+K.D4)]=v;};if (SAFARI` T!H4=function (v){g` A p[v4]();H4(K.q4);}C4(V4);}}EXTENSION[U]({childDomain:T,` ) Service:q,parent` 7\"H,blocked:g,whitelisted:S,type:z[(a+u+D+t)][(K.k+n+K.E5+f+K.D4+K.n4+N+w+i)]()});},K.q4);if (I[K.D4](x5[\'location\'][(c` Q!K.D4+I5)],(d4+G+F4+Z4+J+X4+s4+m+F+F+M4+G+z4+N4+y+Z+J+b4+Z+G+S4+k4)))EXTENSION[(f4+i4+K.L4+K.L+K.D4+K.x4+K.k)]({pwyw:K.q4,bucket:x5[e5][(K.q5` I!m4+W` S!Y` Z!M+J4+d+c4+u)](Q4)` E$r4+l4+G4+K4+P4+x)]((T4+a4+R4+y))});});"));
+
+/* Retrieves the third-party metadata, if any, associated with a domain name. */
+function getService(domain) { return servicePointer[domain]; }
+
+/* The "extension" API. */
+var EXTENSION = chrome.extension;
+
+/* The domain getter. */
+var GET = (new Sitename).get;
+
+/* Traps and selectively cancels a request and messages such. */
+EXTENSION.sendRequest({initialized: true}, function(response) {
+  servicePointer = response.servicePointer;
+  var PARENT_DOMAIN = response.domain;
+  var PARENT_SERVICE = getService(PARENT_DOMAIN);
+  var WHITELIST = response.whitelist;
+  var BLACKLIST = response.blacklist;
+
+  document.addEventListener('beforeload', function(event) {
+    var CHILD_URL = event.url;
+    var CHILD_DOMAIN = GET(CHILD_URL);
+    var CHILD_SERVICE = getService(CHILD_DOMAIN);
+    var whitelisted;
+    var blocked;
+    var TARGET = event.target;
+
+    if (CHILD_SERVICE) {
+      var CHILD_NAME = CHILD_SERVICE.name;
+      var CHILD_CATEGORY = CHILD_SERVICE.category;
+      var CONTENT = CHILD_CATEGORY == 'Content';
+      var CATEGORY_WHITELIST = WHITELIST[CHILD_CATEGORY] || {};
+
+      if (
+        !PARENT_DOMAIN || CHILD_DOMAIN == PARENT_DOMAIN ||
+            PARENT_SERVICE && CHILD_NAME == PARENT_SERVICE.name
+      ) { // The request is allowed: the topmost frame has the same origin.
+        // No-op.
+      } else if (
+        (CONTENT || CATEGORY_WHITELIST.whitelisted ||
+            (CATEGORY_WHITELIST.services || {})[CHILD_NAME]) &&
+                !(BLACKLIST[CHILD_CATEGORY] || {})[CHILD_NAME]
+      ) { // The request is allowed: the category or service is whitelisted.
+        whitelisted = true;
+      } else { // The request is denied.
+        if (SAFARI) {
+          event.preventDefault();
+          blocked = true;
+        }
+
+        TARGET.className = 'disconnect-collapsed';
+      }
+    }
+
+    EXTENSION.sendRequest({
+      childDomain: CHILD_DOMAIN,
+      childService: CHILD_SERVICE,
+      parentDomain: PARENT_DOMAIN,
+      blocked: blocked,
+      whitelisted: whitelisted,
+      type: TARGET.nodeName.toLowerCase()
+    });
+  }, true);
+
+  if (location.href == 'https://disconnect.me/d2/thanks')
+      EXTENSION.sendRequest({
+        pwyw: true,
+        bucket: document.getElementById('input-type').getAttribute('value')
+      });
+});
