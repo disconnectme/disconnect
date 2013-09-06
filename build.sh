b988364eee727f706c56a330c70fb2c5fec67eb4
Brian Kennish <byoogle@gmail.com>
2013-09-05 19:57:34 -0700

    Builds.

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
-#!/bin/bash          
-( 
-	cd firefox/; zip -r ../builds/FIREFOX-DISCONNECT.xpi * ;
-	cd content/disconnect.safariextension/; zip -r ../../../builds/SAFARI-DISCONNECT.zip * ;
-	cd opera/; zip -r ../../../../builds/OPERA-DISCONNECT.zip * ;
-	cd chrome/; zip -r ../../../../../builds/CHROME-DISCONNECT.zip * ;
-)
+#!/bin/bash
+# A script that packages Disconnect for distribution.
+#
+# Copyright 2013 Disconnect, Inc.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors (one per line):
+#
+#   Eason Goodale <eason.goodale@gmail.com>
+#   Brian Kennish <byoogle@gmail.com>
+cd builds
+rm disconnect.xpi disconnect.zip
+cd ../firefox
+zip -r ../builds/disconnect.xpi * -x \*.DS_Store
+cd content/disconnect.safariextension/opera
+zip -r ../../../../builds/disconnect chrome -x \*.DS_Store
