db69ae481aac364516812524e93698baa24f7c5b
Brian Kennish <byoogle@gmail.com>
2013-11-07 17:18:15 -0800

    Firefox build.

--- firefox/content/overlay.js
+++ firefox/content/overlay.js
@@ -842,7 +842,7 @@ if (typeof Disconnect == 'undefined') {
       var highlightedName = this.highlightedName;
       var clickName = this.clickName;
       var imageExtension = this.imageExtension;
-      var currentBuild = 20;
+      var currentBuild = 21;
       var previousBuild = preferences.getIntPref(buildName);
       var whitelist = JSON.parse(preferences.getCharPref(whitelistName));
       var browsingHardened = preferences.getBoolPref(browsingHardenedName);
