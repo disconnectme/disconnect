9574fdd132f2ec5f1c8ebc6eacceec402beda061
Eason Goodale <eason.goodale@gmail.com>
2013-12-06 15:46:53 -0800

    Revert "prep for growl notification"
    This reverts commit 5ec58e35eaf80f320a75cd9a7dff3c99639f3605.

--- firefox/content/disconnect.safariextension/opera/chrome/scripts/content.js
+++ firefox/content/disconnect.safariextension/opera/chrome/scripts/content.js
@@ -91,7 +91,4 @@ EXTENSION.sendRequest({initialized: true}, function(response) {
       BUCKET && EXTENSION.sendRequest({pwyw: true, bucket: BUCKET});
     });
   }
-  EXTENSION.sendRequest({ecpa: true}, function(response) {
-    //growl notification?
-  });
 });
