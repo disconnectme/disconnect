f0f79f6a3661698c27d0aa29ca0cb44a173225a4
Eason Goodale <eason.goodale@gmail.com>
2014-05-22 16:10:56 -0700

    bump

--- firefox/content/disconnect.safariextension/opera/chrome/scripts/background.js
+++ firefox/content/disconnect.safariextension/opera/chrome/scripts/background.js
@@ -312,7 +312,7 @@ if (SAFARI)
     }
 
 /* The current build number. */
-const CURRENT_BUILD = 76;
+const CURRENT_BUILD = 77;
 
 /* The previous build number. */
 const PREVIOUS_BUILD = options.build;
