920b232d66c13e81ca5f8ca003a7b907a7302dfa
Eason Goodale <eason.goodale@gmail.com>
2014-06-06 16:00:05 -0700

    skeleton of settings page

---
+++ firefox/content/disconnect.safariextension/opera/chrome/scripts/settings.js
@@ -0,0 +1,51 @@
+/* The input keyword. */
+const INPUT = 'input';
+
+$(function() {
+  const INDICATOR = $('#indicator ' + INPUT)[0];
+  INDICATOR.checked = DESERIALIZE(options.blockingIndicated);
+  $(INDICATOR).off('click');
+
+  INDICATOR.onclick = function() {
+    const BLOCKING_INDICATED = DESERIALIZE(options.blockingIndicated);
+    this.checked = options.blockingIndicated = !BLOCKING_INDICATED;
+
+    if (BLOCKING_INDICATED) {
+      TABS.query({}, function(tabs) {
+        const TAB_COUNT = tabs.length;
+        for (var i = 0; i < TAB_COUNT; i++)
+            BACKGROUND.chrome.browserAction.setBadgeText({
+              tabId: tabs[i].id, text: ''
+            });
+      });
+    }
+  };
+
+  const CAP = $('#cap ' + INPUT)[0];
+  CAP.checked = DESERIALIZE(options.blockingCapped);
+  $(CAP).off('click');
+
+  CAP.onclick = function() {
+    this.checked =
+        options.blockingCapped = !DESERIALIZE(options.blockingCapped);
+  };
+
+  const NOTIFICATION = $('#notifications ' + INPUT)[0];
+  NOTIFICATION.checked = DESERIALIZE(options.notificationsEnabled);
+  $(NOTIFICATION).off('click');
+
+  NOTIFICATION.onclick = function() {
+    this.checked =
+        options.notificationsEnabled = !DESERIALIZE(options.notificationsEnabled);
+  };
+
+  const SURROGATES = $('#surrogates ' + INPUT)[0];
+  SURROGATES.checked = DESERIALIZE(options.surrogatesEnabled);
+  $(SURROGATES).off('click');
+
+  SURROGATES.onclick = function() {
+    this.checked =
+        options.surrogatesEnabled = !DESERIALIZE(options.surrogatesEnabled);
+  };
+
+});
